f1cee83cfc409d41759110fcc001bde75853f5b8
Vicktor007 <vicktord007@gmail.com>
2024-05-19 11:07:21 +0100

    button issue resolved

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -92,6 +92,7 @@ const MeetingRoom = () => {
         <button type='button' onClick={() => setShowParticipants((prev) => !prev)}>
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
+            <p className='hidden'>users</p>
           </div>
         </button>
         {!isPersonalRoom && <EndCallButton />}
