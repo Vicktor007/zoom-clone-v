adb36c999976bf201ff8ce0c5dfd4a2f1acf2bd7
Vicktor007 <vicktord007@gmail.com>
2024-11-22 11:50:59 +0100

    save

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,6 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp />
       <SignUp path="/sign-up" routing="path" fallbackRedirectUrl="/"  />
     </main>
   );
